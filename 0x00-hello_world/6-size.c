0776c8234581dcb97325b226d11ffb1d8126e06a
leidy perez <leidyperez64@hotmail.com>
2023-02-08 16:31:15 +0000

    Fix printf message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(longlongType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 	return (0);
 }
