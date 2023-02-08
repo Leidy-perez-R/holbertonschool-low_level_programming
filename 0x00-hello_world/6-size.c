6c5485c2cf5637e6f14ff8a72f60c1408673c2a2
leidy perez <leidyperez64@hotmail.com>
2023-02-08 16:26:10 +0000

    Fix printf message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	char charType;
 	int intType;
 	long int longintType;
-	long long int longlongintType;
+	long long longlongType;
 	float floatType;
 
-	printf("Size of char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of int: %lu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %lu byte(s)\n", sizeof(longintType));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(longlongintType));
-	printf("Size of float: %lu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long: %lu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 	return (0);
 }
