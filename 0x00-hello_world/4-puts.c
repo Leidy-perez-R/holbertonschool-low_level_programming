37bde27720f54e9b931338f6b2d80d914bf504d2
leidy perez <leidyperez64@hotmail.com>
2023-02-08 14:07:49 +0000

    Fix identation spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+/**
+ * main - my principal function main
+ *
+ * Return: Return 0 when finished
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
-
-
