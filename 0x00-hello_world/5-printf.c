1641e6a7b9ac565275e350247e01ce2add9c7995
leidy perez <leidyperez64@hotmail.com>
2023-02-08 14:36:08 +0000

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - its a principal main
+ *
+ *Return: Return 0 when finished
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art\n");
+	return (0);
+}
